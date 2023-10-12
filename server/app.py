0a2cc0dcf38410fe648aaf0c4c74f366a39d77d8
john makateto <jmakateto@gmail.com>
2023-10-13 00:37:32 +0300

    made changes

--- server/app.py
+++ server/app.py
@@ -2,9 +2,8 @@
 
 from flask import Flask, request, make_response
 from flask_marshmallow import Marshmallow
-from flask_migrate import Migrate
+from flask_migrate import Migrate  
 from flask_restful import Api, Resource
-
 from models import db, Newsletter
 
 app = Flask(__name__)
@@ -12,111 +11,88 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///newsletters.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
+# Initialize Migrate object
 migrate = Migrate(app, db)
+
 db.init_app(app)
 
 api = Api(app)
+ma = Marshmallow(app)
+
+class NewsletterSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = Newsletter
+        load_instance = True
+
+    title = ma.auto_field()
+    published_at = ma.auto_field()
+
+    url = ma.Hyperlinks(
+        {
+            "self": ma.URLFor(
+                "newsletterbyid",
+                values=dict(id="<id>")),
+            "collection": ma.URLFor("newsletters"),
+        }
+    )
+
+newsletter_schema = NewsletterSchema()
+newsletters_schema = NewsletterSchema(many=True)
 
 class Index(Resource):
-
     def get(self):
-        
         response_dict = {
             "index": "Welcome to the Newsletter RESTful API",
         }
-        
-        response = make_response(
-            response_dict,
-            200,
-        )
-
+        response = make_response(response_dict, 200)
         return response
 
-api.add_resource(Index, '/')
-
 class Newsletters(Resource):
-
     def get(self):
-        
-        response_dict_list = [n.to_dict() for n in Newsletter.query.all()]
-
-        response = make_response(
-            response_dict_list,
-            200,
-        )
-
+        newsletters = Newsletter.query.all()
+        response_dict_list = [newsletter_schema.dump(n) for n in newsletters]
+        response = make_response(response_dict_list, 200)
         return response
 
     def post(self):
-        
         new_record = Newsletter(
             title=request.form['title'],
             body=request.form['body'],
         )
-
         db.session.add(new_record)
         db.session.commit()
-
-        response_dict = new_record.to_dict()
-
-        response = make_response(
-            response_dict,
-            201,
-        )
-
+        response_dict = newsletter_schema.dump(new_record)
+        response = make_response(response_dict, 201)
         return response
 
-api.add_resource(Newsletters, '/newsletters')
-
 class NewsletterByID(Resource):
-
     def get(self, id):
-
-        response_dict = Newsletter.query.filter_by(id=id).first().to_dict()
-
-        response = make_response(
-            response_dict,
-            200,
-        )
-
+        newsletter = Newsletter.query.filter_by(id=id).first()
+        response_dict = newsletter_schema.dump(newsletter)
+        response = make_response(response_dict, 200)
         return response
 
     def patch(self, id):
-
         record = Newsletter.query.filter_by(id=id).first()
         for attr in request.form:
             setattr(record, attr, request.form[attr])
-
         db.session.add(record)
         db.session.commit()
-
-        response_dict = record.to_dict()
-
-        response = make_response(
-            response_dict,
-            200
-        )
-
+        response_dict = newsletter_schema.dump(record)
+        response = make_response(response_dict, 200)
         return response
 
     def delete(self, id):
-
         record = Newsletter.query.filter_by(id=id).first()
-        
         db.session.delete(record)
         db.session.commit()
-
         response_dict = {"message": "record successfully deleted"}
-
-        response = make_response(
-            response_dict,
-            200
-        )
-
+        response = make_response(response_dict, 200)
         return response
 
+api.add_resource(Index, '/')
+api.add_resource(Newsletters, '/newsletters')
 api.add_resource(NewsletterByID, '/newsletters/<int:id>')
-
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
